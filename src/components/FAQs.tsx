5563a2163754afbb678b90d9c93d8159bb8540fd
Luke Braithwaite <lukebraithwaite7@gmail.com>
2022-10-22 18:29:18 +0100

    🎉  Initial commit

---
+++ src/components/FAQs.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Disclosure } from '@headlessui/react';
+import { ChevronUpIcon } from '@heroicons/react/20/solid';
+import QuestionData from '@data/faqs.json';
+
+const FAQs = (): JSX.Element => {
+	return (
+		<div id='faqs' className='text-black mt-16 mx-auto w-full px-4 max-w-screen-2xl lg:px-8'>
+			<h1 className='text-5xl text-center font-semibold mb-8'>FAQs</h1>
+			<div className='hidden lg:grid grid-cols-3 gap-4 '>
+				{QuestionData.map(({ question, answer }, i) => (
+					<div className='rounded shadow-md px-6 py-4' key={`faq_desktop_${i}`}>
+						<h1 className='text-xl font-bold mb-2 text-gray-900'>{question}</h1>
+						<p className='text-lg text-gray-500'>{answer}</p>
+					</div>
+				))}
+			</div>
+			<div className='flex flex-col space-y-3 lg:hidden'>
+				{QuestionData.map(({ question, answer }, i) => (
+					<Disclosure as={'div'} className='rounded shadow-md px-6 py-4' key={`faq_mobile_${i}`}>
+						<Disclosure.Button
+							className={'flex w-full justify-between items-center text-left text-xl font-medium text-gray-900'}
+						>
+							{question}
+							<ChevronUpIcon className='ui-open:rotate-180 ui-open:transform h-6 w-6 text-gray-900' />
+						</Disclosure.Button>
+						<Disclosure.Panel className={'pt-4 px-2 text-lg text-gray-500'}>{answer}</Disclosure.Panel>
+					</Disclosure>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default FAQs;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import GUHLogo from '../assets/guh.svg';
+
+const NavBar = (): JSX.Element => {
+	return (
+		<nav className='bg-guh-orange flex items-center justify-between py-4 px-32'>
+			<img src={GUHLogo} className='h-20' />
+			<ul className='flex flex-row items-baseline space-x-8 text-lg font-medium'>
+				<li>
+					<a href='#faqs'>FAQ</a>
+				</li>
+				<li>About</li>
+				<li>Sponsors</li>
+				<li>Contact</li>
+				<li>Register Now</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
