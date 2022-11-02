28ca444a3463e4ec1a60e62de86145a2c5998272
Sambbhav Khare <sambbhav.khare@student.manchester.ac.uk>
2022-11-02 23:18:22 +0000

    Sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,12 +1,134 @@
 import Layout from './Layout';
 import sponsors from '../assets/sponsors.png';
+import palantir from '../assets/Sponsors/Palantir.png';
+import devotee1 from '../assets/Sponsors/devotee1.png';
+import devotee2 from '../assets/Sponsors/devotee2.png';
+import devotee3 from '../assets/Sponsors/devotee3.png';
+import enthusiast1 from '../assets/Sponsors/enthusiast1.png';
+import enthusiast2 from '../assets/Sponsors/enthusiast2.png';
+import advocate1 from '../assets/Sponsors/advocate1.png';
+import partner1 from '../assets/Sponsors/partner1.png';
+import partner2 from '../assets/Sponsors/partner2.png';
+import partner3 from '../assets/Sponsors/partner3.png';
+import partner4 from '../assets/Sponsors/partner4.png';
+
+// import sponsor from '@data/sponsor.json';
+
 
 const Sponsors = (): JSX.Element => {
 	return (
 		<Layout title='Sponsors'>
-			<img className='w-full' src={sponsors} />
+			
+			<h2 className='flex flex-col mx-auto  items-center'>Forerunner</h2>
+			<div className='grid-gap: 20px grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-1 gap-4 items-center'>
+			<a href="https://www.palantir.com/uk/careers/">
+				 <img className='h-36 w-50 hover:-translate-y-1' src={palantir} />
+			</a>
+			</div>
+			<h2 className='flex flex-col mx-auto  items-center'>Devotee</h2>
+			<div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4'>
+				<a href="https://careers.booking.com/?utm_source=corporate&utm_medium=footer">
+							<img className='hover:-translate-y-1' src={devotee1} />
+				</a>
+				<a href="https://www.bet365careers.com/en/current-vacancies">
+							<img className='hover:-translate-y-1' src={devotee2} />
+				</a>
+				<a href="https://netcraft.com/jobs">
+							<img className='hover:-translate-y-1' src={devotee3} />
+				</a> 
+			</div>
+			<h2 className='flex flex-col mx-auto items-center'>Enthusiast</h2>
+			<div className='grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-4'>
+				<a href="https://www.mwam.com/join-us/">
+							<img className='h-50 w-full hover:-translate-y-1' src={enthusiast1} />
+				</a>
+				<a href="https://www.recourseai.com/">
+							<img className='w-full hover:-translate-y-1' src={enthusiast2} />
+				</a> 
+			</div>
+			<h2 className='flex flex-col mx-auto items-center'>Advocate</h2>
+			<div className='grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4'>
+				<a href="https://www.atmosi.com/en">
+							<img className='h-36 w-50 hover:-translate-y-1' src={advocate1} />
+				</a>
+			</div>
+			<h2 className='flex flex-col mx-auto  items-center'>Partners</h2>
+			<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
+				<a href="https://www.mmu.ac.uk/">
+							<img className=' h-36 w-50 hover:-translate-y-1' src={partner1} />
+				</a>	
+				<a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intro">
+							<img className=' h-36 w-50 hover:-translate-y-1' src={partner4} />	
+				</a>
+				<a href="https://mlh.io/">
+							<img className=' h-36 w-50 hover:-translate-y-1' src={partner2} />	
+				</a>
+				<a href="https://hackathons.org.uk/">
+							<img className=' h-36 w-50 hover:-translate-y-1' src={partner3} />
+				</a>
+			</div>
+			
 		</Layout>
 	);
 };
 
 export default Sponsors;
+
+
+// **Forerunner**
+
+// Palantir
+// https://www.palantir.com/uk/careers/
+// https://drive.google.com/file/d/1p3yiVT8keP_AB0zNEUQPqBt0urr95SRP/view?usp=sharing
+
+
+// **Devotee**
+
+// Booking.com
+// https://careers.booking.com/?utm_source=corporate&utm_medium=footer
+// https://drive.google.com/drive/folders/1qo4LOYbsL6vW2QssAnaQKiLTa_BQtqPU?usp=sharing
+
+// Bet365
+// https://www.bet365careers.com/en/current-vacancies
+// https://drive.google.com/drive/folders/1kfKMdawu2MeWBN7rW8lfKwwgsByMSFlZ?usp=sharing
+
+// Netcraft
+// https://netcraft.com/jobs
+// https://drive.google.com/file/d/1cwRFcGGZ-vlubUEWj6vEFE0H-hralXNp/view?usp=sharing
+
+
+// **Enthusiast**
+
+// Marshall Wace
+// https://www.mwam.com/join-us/
+// https://drive.google.com/drive/folders/1N4ii8eYw5XvGpfdwn5rhkos8J_1IKAQJ?usp=sharing
+
+// Re:course AI
+// https://www.recourseai.com/
+// https://drive.google.com/file/d/1mh893ZWJYxyKyxnZiND1xTQsZ1Wgn6f0/view?usp=sharing
+
+
+// **Advocate**
+
+// Atmos International
+// https://www.atmosi.com/en
+// https://drive.google.com/drive/folders/1ePbjbcyv7zpfq-2_ye0Mj4VEaBFy5-QJ?usp=sharing
+
+
+// **Partners**
+
+// Manchester Metropolitan University
+// https://www.mmu.ac.uk/
+// https://drive.google.com/file/d/1tPnlvkAMOeAkgjYGeMACzhOuJ4TFjmK-/view?usp=sharing
+
+// Major League Hacking
+// https://mlh.io/
+// https://drive.google.com/drive/folders/14kb-KcDiwevnEKoEgRzjBPiualtkBady?usp=sharing
+
+// Hackathons UK
+// https://hackathons.org.uk/
+// https://drive.google.com/drive/folders/1m-J7a4nheHXJ-9B3R-_EemhpCZifmNno?usp=sharing
+
+// StandOut Stickers
+// https://www.standoutstickers.com/?utm_campaign=events-league-organizers-spring2022&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intro
+// https://drive.google.com/file/d/1mKFvD5LYeX3R7IVwiVHRX4AUI5UJAM5S/view?usp=sharing
